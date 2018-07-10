2108ddee9d3c13500d0293bee6ba109ca47266e7
Eric Lee <ll298lee@gmail.com>
2018-07-10 13:55:17 +0800

    fix import

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -9,6 +9,8 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 
 class FastImageViewModule extends ReactContextBaseJavaModule {
 

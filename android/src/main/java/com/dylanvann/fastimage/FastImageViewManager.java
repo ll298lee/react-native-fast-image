4e87e9730f67592951bdb0cd6df55bbebe9bd7ba
Eric Lee <ll298lee@gmail.com>
2018-07-13 15:21:49 +0800

    fix context cast crash

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
@@ -40,7 +40,7 @@ class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl> imple
     @Override
     protected FastImageViewWithUrl createViewInstance(ThemedReactContext reactContext) {
         requestManager = Glide.with(reactContext.getApplicationContext());
-        return new FastImageViewWithUrl(reactContext.getApplicationContext());
+        return new FastImageViewWithUrl(reactContext);
     }
 
     @ReactProp(name = "source")
